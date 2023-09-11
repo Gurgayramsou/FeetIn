f54236ecb17b9002ef97f655946b024bfa04c975
Gurgayramsou <hegderam17@gmail.com>
2023-09-11 13:05:19 +0530

    modal feature added

--- src/home/home.js
+++ src/home/home.js
@@ -8,3 +8,11 @@ userIconElem.addEventListener("mouseover", () => {
 setInterval(() => {
   optionDropDown.style.visibility = "hidden";
 }, 5000);
+
+const modalBack = document.querySelector(".modalBack");
+document.querySelector("#signOut").addEventListener("click", () => {
+  modalBack.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+  modalBack.style.zIndex = "20";
+  document.querySelector(".modalWindow").style.zIndex = "30";
+  return false;
+});
